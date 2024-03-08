d99774f448f393539e323d8ec5cedd6ee5b1f856
Imran <imran40031@gmail.com>
2024-03-08 12:39:21 +0600

    [Update]

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,47 @@
+import 'dart:async';
+
 import 'package:block_test/screen/bloc/counter/counter_block.dart';
 
 import 'package:block_test/screen/bloc/image_picker/bloc/image_picker_bloc.dart';
 import 'package:block_test/screen/bloc/switch/switch_block.dart';
 import 'package:block_test/screen/bloc/todo/todo_bloc.dart';
+import 'package:block_test/screen/ui/home/home_screen.dart';
 
 import 'package:block_test/screen/ui/switch_ui/switch_view.dart';
 import 'package:block_test/utils/image_picker_utils.dart';
+import 'package:block_test/utils/local_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+int id = 0;
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+final StreamController<ReceivedNotification> didReceiveLocalNotificationStream =
+    StreamController<ReceivedNotification>.broadcast();
+final StreamController<String?> selectNotificationStream =
+    StreamController<String?>.broadcast();
+
+class ReceivedNotification {
+  ReceivedNotification({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.payload,
+  });
+
+  final int id;
+  final String? title;
+  final String? body;
+  final String? payload;
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await LocalNotificaitons.init();
+
+  // await _configureLocalTimeZone();
   runApp(const MyApp());
 }
 
@@ -23,11 +55,11 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider(create: (_) => CounterBloc()),
         BlocProvider(create: (context) => SwitchBlock()),
-        BlocProvider(create: (context) => ImagePickerBloc(imagePickerUtils: ImagePickerUtils())),
+        BlocProvider(
+            create: (context) =>
+                ImagePickerBloc(imagePickerUtils: ImagePickerUtils())),
         BlocProvider(create: (context) => TodoBloc()),
         // BlocProvider(create: (context) => FavouriteBloc(FavouriteRepository())),
-
-        
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
@@ -35,8 +67,69 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const SwitchView(),
+        home: const HomeScreen(),
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:socket_io_client/socket_io_client.dart' as IO;
+// import 'package:socket_io_client/socket_io_client.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   IO.Socket? socket;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Connect to Socket.IO server
+    
+//     Socket socket = io('http://localhost:3000', 
+//     OptionBuilder()
+//       .setTransports(['websocket']) // for Flutter or Dart VM
+//       .enableAutoConnect()  // disable auto-connection
+//       .setExtraHeaders({'foo': 'bar'}) // optional
+//       .build()
+//   );
+//       socket.onConnect((_) {
+//       print('connect');
+//     });
+//     socket.connect();
+
+
+//     // // Connect to the server
+//     // socket!.connect();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Socket.IO Notifications Demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Socket.IO Notifications Demo'),
+//         ),
+//         body: Center(
+//           child: Text('Listening for notifications...'),
+//         ),
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     // Disconnect the socket when the app is closed
+//     socket!.disconnect();
+//   }
+// }
---
+++ lib/screen/ui/home/home_screen.dart
@@ -0,0 +1,136 @@
+import 'dart:io';
+
+import 'package:block_test/utils/local_notifications.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+import '../../../main.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    _isAndroidPermissionGranted();
+    _requestPermissions();
+    _configureDidReceiveLocalNotificationSubject();
+    _configureSelectNotificationSubject();
+    super.initState();
+  }
+
+  bool _notificationsEnabled = false;
+
+  Future<void> _isAndroidPermissionGranted() async {
+    if (Platform.isAndroid) {
+      final bool granted = await flutterLocalNotificationsPlugin
+              .resolvePlatformSpecificImplementation<
+                  AndroidFlutterLocalNotificationsPlugin>()
+              ?.areNotificationsEnabled() ??
+          false;
+
+      setState(() {
+        _notificationsEnabled = granted;
+      });
+    }
+  }
+
+  Future<void> _requestPermissions() async {
+    if (Platform.isIOS || Platform.isMacOS) {
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              IOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+            alert: true,
+            badge: true,
+            sound: true,
+          );
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+              MacOSFlutterLocalNotificationsPlugin>()
+          ?.requestPermissions(
+            alert: true,
+            badge: true,
+            sound: true,
+          );
+    } else if (Platform.isAndroid) {
+      final AndroidFlutterLocalNotificationsPlugin? androidImplementation =
+          flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+              AndroidFlutterLocalNotificationsPlugin>();
+
+      final bool? grantedNotificationPermission =
+          await androidImplementation?.requestNotificationsPermission();
+      setState(() {
+        _notificationsEnabled = grantedNotificationPermission ?? false;
+      });
+    }
+  }
+
+  void _configureDidReceiveLocalNotificationSubject() {
+    didReceiveLocalNotificationStream.stream
+        .listen((ReceivedNotification receivedNotification) async {
+      await showDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+          title: receivedNotification.title != null
+              ? Text(receivedNotification.title!)
+              : null,
+          content: receivedNotification.body != null
+              ? Text(receivedNotification.body!)
+              : null,
+          actions: <Widget>[
+            CupertinoDialogAction(
+              isDefaultAction: true,
+              onPressed: () async {
+                // Navigator.of(context, rootNavigator: true).pop();
+                // await Navigator.of(context).push(
+                //   MaterialPageRoute<void>(
+                //     builder: (BuildContext context) => SecondPage(receivedNotification.payload),
+                //   ),
+                // );
+              },
+              child: const Text('Ok'),
+            )
+          ],
+        ),
+      );
+    });
+  }
+
+  void _configureSelectNotificationSubject() {
+    selectNotificationStream.stream.listen((String? payload) async {
+      // await Navigator.of(context).push(MaterialPageRoute<void>(
+      //   builder: (BuildContext context) => SecondPage(payload),
+      // ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Screen"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                LocalNotificaitons.showSimpleNotification(
+                    title: "Test", body: "Test body", payload: "discription");
+              },
+              child: const Text("Notification"))
+        ],
+      ),
+    );
+  }
+}
